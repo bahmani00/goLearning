69fac920198a3223b10bc6c91de6466d5f2feb16
bahmani00 <bahmani00@gmail.com>
2022-01-16 16:58:13 -0500

    using arrays & slices

--- array/sum.go
+++ array/sum.go
@@ -1,9 +1,26 @@
 package sum
 
+import "fmt"
+
 func Sum(nums []int) int {
 	var sum int
 	for _, number := range nums {
 		sum += number
 	}
 	return sum
+}
+
+func PlayWithArrays() {
+	//https://stackoverflow.com/a/21722697/336511
+	a1 := [3]int{1, 2, 3} // Array literal(value)
+	b1 := a1              // Copy the contents of a into b
+	a1[0] = 0
+	fmt.Println(a1) // Prints "[0 2 3]"
+	fmt.Println(b1) // Prints "[1 2 3]"
+
+	a2 := []int{1, 2, 3} // Slice literal(reference)
+	b2 := a2             // a and b now point to the same memory
+	a2[0] = 0
+	fmt.Println(a2) // Prints "[0 2 3]"
+	fmt.Println(b2) // Prints "[0 2 3]"
 }
--- array/sum_test.go
+++ array/sum_test.go
@@ -5,9 +5,9 @@ import "testing"
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
 
-		numbers := [...]int{1, 2, 3, 4, 5}
-    //Create a slice out of the array when calling the function
-		got := Sum(numbers[:])
+		numbers := [5]int{1, 2, 3, 4, 5} //array literal
+		//Create a slice out of the array when calling the function
+		got := Sum(numbers[:]) //numbers[:] : slice literal
 		expected := 15
 
 		if got != expected {
