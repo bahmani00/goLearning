f2d7b100f601c365977f2f73afe0a3ec30814c5d
bahmani00 <bahmani00@gmail.com>
2022-01-16 17:24:49 -0500

    Write enough code to make it pass

--- array/sum_arrays.go
+++ array/sum_arrays.go
@@ -1,5 +1,11 @@
 package sum
 
-func SumAll(arrayOfNums ...[]int) (sums []int) {
-	return
+func SumAll(arrayOfNums ...[]int) []int {
+	lengthOfArrays := len(arrayOfNums)
+	sums := make([]int, lengthOfArrays)
+
+	for i, nums := range arrayOfNums {
+		sums[i] = Sum(nums)
+	}
+	return sums
 }
