ff7370cd20f6904adbbc20abc1621192de0378a1
bahmani00 <bahmani00@gmail.com>
2022-01-16 11:16:09 -0500

    added spanish lang support

--- hello.go
+++ hello.go
@@ -3,13 +3,22 @@ package main
 import "fmt"
 
 const helloPrefix = "Hello, "
+const helloPrefix_Spanish = "Hola, "
+const lang_sp = "sp"
 
-func Hello(name string) string {
+
+func Hello(name string, lang string) string {
 	if name == "" {
 		name = "World"
 	}
-  return helloPrefix + name
+
+	prefix := helloPrefix;
+	if lang == lang_sp {
+		prefix = helloPrefix_Spanish
+	}
+
+  return prefix + name
 }
 func main()  {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello(t * testing.T)  {
   t.Run("Saying hello to people", func (t * testing.T)  {
-    got := Hello("Chris")
+    got := Hello("Chris", "")
     want := "Hello, Chris"
 
     if got != want {
@@ -13,8 +13,17 @@ func TestHello(t * testing.T)  {
   })
 
   t.Run("Saying hello to nobody", func (t * testing.T)  {
-    got := Hello("")
+    got := Hello("", "")
     want := "Hello, World"
+
+    if got != want {
+      t.Errorf("got %q wanted %q", got, want)
+    }
+  })
+
+  t.Run("Saying hello to people in Spanish", func (t * testing.T)  {
+    got := Hello("Chris", "sp")
+    want := "Hola, Chris"
 
     if got != want {
       t.Errorf("got %q wanted %q", got, want)
