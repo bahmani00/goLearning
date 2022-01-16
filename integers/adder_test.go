09a43182ce94cd35684877713f183919c1e49014
bahmani00 <bahmani00@gmail.com>
2022-01-16 13:19:53 -0500

    added more test cases

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -3,11 +3,20 @@ package integers
 import "testing"
 
 func TestAdder(t *testing.T) {
-	sum := Add(2, 2)
-	expected := 4
+	t.Run("Calc sum of 2 & 2", func(t *testing.T) {
+		sum := Add(2, 2)
+		expected := 4
 
-	if sum != expected {
-		t.Errorf("expected '%d' but got %d", expected, sum)
-	}
+		if sum != expected {
+			t.Errorf("expected %d but got %d", expected, sum)
+		}
+	})
+	t.Run("Calc sum of 2 & 3", func(t *testing.T) {
+		sum := Add(2, 3)
+		expected := 5
+
+		if sum != expected {
+			t.Errorf("expected %d but got %d", expected, sum)
+		}
+	})
 }
-
